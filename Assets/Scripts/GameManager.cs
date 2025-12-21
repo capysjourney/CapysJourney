77ea6931d18b82bbe272e5917675712c27faaa0a
Caleb Shim <calebshim248@gmail.com>
2025-12-21 14:39:15 -0500

    Complete Firebase setup

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.Rendering.Universal.Internal;
 
 public static class GameManager
 {
